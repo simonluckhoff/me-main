cef8fa4bb225a322044aa0ed4d0297283cdd6c9b
Jadon Hansen <jadonhansen@gmail.com>
2024-09-19 11:30:12 +0200

    images working

---
+++ src/work.ts
@@ -0,0 +1,313 @@
+import latest1 from "./images/low_res/992 GT3RS - Airport/1 - 992 GT3RS, Simon, 2024-25.jpg";
+import latest2 from "./images/low_res/Veyron/1 - Veyron, Simon, 2024-10.jpg";
+import latest3 from "./images/low_res/Huracan Perf/1 - Huracan Perf, Simon, 2024-10.jpg";
+
+import recent1 from "./images/low_res/296GTB (Signal Hill)/1 - 296GTB, Simon, 2024-3.jpg";
+import recent2 from "./images/low_res/Evan's Pista/1 - Pista, Simon, 2024-11.jpg";
+import recent3 from "./images/low_res/488 Spider/1 - 488Spider, Simon, 2024-3.jpg";
+import recent4 from "./images/low_res/SVJ/1 - SVJ, Simon, 2024-10.jpg";
+import recent5 from "./images/low_res/720S/1 - 720S, Simon, 2024-13.jpg";
+import recent6 from "./images/low_res/The Archive/1 - Ferrrari, Simon, 2022.jpg";
+import recent7 from "./images/low_res/812 GTS A/1 - 812 GTS A, Simon, 2024-37.jpg";
+import recent8 from "./images/low_res/992 GT3RS - Mike/1 - 992 GT3RS - Mike, Simon, 2024-28.jpg";
+import recent9 from "./images/low_res/911 GT3/1 - 911 GT3, Simon, 2024-38.jpg";
+import recent10 from "./images/low_res/992 GT3/1 - 992 GT3, Simon, 2024-30.jpg";
+import recent11 from "./images/low_res/Pista Elana/1 - Pista - Elana, Simon, 2024-52.jpg";
+import recent12 from "./images/low_res/996 GT3/1 - 996 GT3, Simon, 2024-5.jpg";
+import recent13 from "./images/low_res/Conrad GTR /1 - GTR, Simon, 2024-21.jpg";
+import recent14 from "./images/low_res/997 GT3RS/1 - 997 GT3RS, Simon, 2024.jpg";
+import recent15 from "./images/low_res/Akra GT3RS/1 - 991 GT3RS, Simon, 2024-36.jpg";
+import recent16 from "./images/low_res/Bentley NBR 9/1 - Bentley NBR 9, Simon, 2024-50.jpg";
+import recent17 from "./images/low_res/Chris Corolla GR/1 - Chris Corolla GR, Simon, 2024-33.jpg";
+import recent18 from "./images/low_res/Comp & 296/1 - Comp & 296, Simon, 2024-13.jpg";
+import recent19 from "./images/low_res/Dakar/1 - 911 Dakar, Simon, 2024-24.jpg";
+import recent20 from "./images/low_res/F8/1 - F8, Simon, 2024-23.jpg";
+import recent21 from "./images/low_res/G63 6x6/1 - G63 6x6, Simon, 2024-10.jpg";
+import recent22 from "./images/low_res/G63/1 - G63, Simon, 2024-10.jpg";
+import recent23 from "./images/low_res/G87 M2/1 - G87 M2, Simon, 2024-11.jpg";
+import recent24 from "./images/low_res/Green GTR/1 - GTR, Simon, 2024-22.jpg";
+import recent25 from "./images/low_res/GT3 Touring/1 - 992 GT3, Simon, 2024.jpg";
+import recent26 from "./images/low_res/GT40/1 - GT40, Simon, 2024-21.jpg";
+import recent27 from "./images/low_res/GTR Christiaan/1 - GTR Christiaan, Simon, 2024-5.jpg";
+import recent28 from "./images/low_res/i8/1 - i8, Simon, 2024-8.jpg";
+import recent29 from "./images/low_res/Lotus/1 - Lotus, Simon, 2024-32.jpg";
+import recent30 from "./images/low_res/Lusso/1 - Lusso, Simon, 2024.jpg";
+import recent31 from "./images/low_res/M3CSL/1 - CSL, Simon, 2024-24.jpg";
+import recent32 from "./images/low_res/M4/1 - M4, Simon, 2024-25.jpg";
+import recent33 from "./images/low_res/Macan T/1 - Macan T, Simon, 2024-10.jpg";
+import recent34 from "./images/low_res/Matte 458/1 - Matte 458, Simon, 2024-22.jpg";
+import recent35 from "./images/low_res/Mustang Mach 1/1 - Mustang Mach 1, Simon, 2024-18.jpg";
+import recent36 from "./images/low_res/Orange GT3RS/1 - Orange GT3RS, Simon, 2024-13.jpg";
+import recent37 from "./images/low_res/Pista - Marco/1 - Pista - Marco, Simon, 2024-2.jpg";
+import recent38 from "./images/low_res/R8 V10+/1 - R8 V10+, Simon, 2024-9.jpg";
+import recent39 from "./images/low_res/R8/1 - R8 Star Wars, Simon, 2024-51.jpg";
+import recent40 from "./images/low_res/Racetrack 992 GT3/1 - 992 GT3 Racetrack, Simon, 2024-9.jpg";
+import recent41 from "./images/low_res/RS6/1 - RS6, Simon, 2024-6.jpg";
+import recent42 from "./images/low_res/Silver GT3RS/1 - Silver GT3RS, Simon, 2024-30.jpg";
+import recent43 from "./images/low_res/SLS AMG/1 - SLS AMG, Simon, 2024-4.jpg";
+import recent44 from "./images/low_res/Speciale - Blue Stripe/1 - Speciale, Simon, 2024-2.jpg";
+import recent45 from "./images/low_res/Supra/1 - Supra, Simon, 2024-30.jpg";
+import recent46 from "./images/low_res/TDF/1 - TDF, Simon, 2024-32.jpg";
+import recent47 from "./images/low_res/White F12/1 - White F12, Simon, 2024-4.jpg";
+import recent48 from "./images/low_res/Zaki E90 M3/1 - E90 M3, Simon, 2024-3.jpg";
+
+export const latestWork = [
+	{
+		image: latest1,
+		title: "Porsche 992 GT3RS",
+		href: "/cars/1-3RSAirport.html",
+	},
+	{
+		image: latest2,
+		title: "Bugatti Veyron",
+		href: "/cars/2-Veyron.html",
+	},
+	{
+		image: latest3,
+		title: "Lamborghini Huracan Performanté",
+		href: "/cars/27-Lamborghini Huracan Performanté.html",
+	},
+];
+
+export const recentWork = [
+	{
+		href: "/cars/4-296.html",
+		image: recent1,
+		title: "Ferrari 296GTB",
+	},
+	{
+		href: "/cars/3-488pista.html",
+		image: recent2,
+		title: "Ferrari 488 Pista",
+	},
+	{
+		href: "/cars/5 - 488Spider.html.html",
+		image: recent3,
+		title: "Ferrari 488 Spider",
+	},
+	{
+		href: "/cars/47-Lamborghini Aventador SVJ.html",
+		image: recent4,
+		title: "Lamborghini Aventador SVJ",
+	},
+	{
+		href: "/cars/6-720s.html",
+		image: recent5,
+		title: "Mclaren 720s",
+	},
+	{
+		href: "/cars/49-The Archive Automotive.html",
+		image: recent6,
+		title: "The Archive Automotive",
+	},
+	{
+		href: "/cars/7-Ferrari812GTSAtelier.html",
+		image: recent7,
+		title: "Ferrari 812 GTS Atelier",
+	},
+	{
+		href: "/cars/10-porsche992gt3rsblack.html",
+		image: recent8,
+		title: "Porsche 992 GT3RS",
+	},
+	{
+		href: "/cars/8-911gt3.html",
+		image: recent9,
+		title: "Porsche 911 GT3",
+	},
+	{
+		href: "/cars/9-992GT3.html",
+		image: recent10,
+		title: "Porsche 992 GT3",
+	},
+	{
+		href: "/cars/38 - elana pista.html",
+		image: recent11,
+		title: "Ferrari 488 Pista",
+	},
+	{
+		href: "/cars/11 - 996 GT3.html",
+		image: recent12,
+		title: "Porsche 996 GT3",
+	},
+	{
+		href: "/cars/17-Mercedes-Benz AMG GTR.html",
+		image: recent13,
+		title: "Mercedes-Benz AMG GTR",
+	},
+	{
+		href: "/cars/12 - Porsche 997.2 GT3RS.html",
+		image: recent14,
+		title: "Porsche 997.2 GT3RS",
+	},
+	{
+		href: "/cars/13-Porsche 991.2 GT3RS.html",
+		image: recent15,
+		title: "Porsche 991.2 GT3RS",
+	},
+	{
+		href: "/cars/14-Bentley Continental GT Number 9.html",
+		image: recent16,
+		title: "Bentley Continental GT Number 9",
+	},
+	{
+		href: "/cars/15 - Toyota GR Corolla .html",
+		image: recent17,
+		title: "Toyota GR Corolla",
+	},
+	{
+		href: "/cars/16 -Ferrari 812 Competizione Aperta.html",
+		image: recent18,
+		title: "Ferrari 812 Competizione Aperta",
+	},
+	{
+		href: "/cars/19-Ferrari F8 Tributo.html",
+		image: recent19,
+		title: "Porsche 992 Dakar",
+	},
+	{
+		href: "/cars/19-Ferrari F8 Tributo.html",
+		image: recent20,
+		title: "Ferrari F8 Tributo",
+	},
+	{
+		href: "/cars/20-Mercedes-Benz G63 AMG 6x6 Brabus.html",
+		image: recent21,
+		title: "Mercedes-Benz AMG G63 6x6 Brabus",
+	},
+	{
+		href: "/cars/21 -Mercedes-Benz AMG G63.html",
+		image: recent22,
+		title: "Mercedes-Benz AMG G63",
+	},
+	{
+		href: "/cars/22-BMW G87 M2.html",
+		image: recent23,
+		title: "BMW G87 M2",
+	},
+	{
+		href: "/cars/23-Mercedes-Benz AMG GTR.html",
+		image: recent24,
+		title: "Mercedes-Benz AMG GTR",
+	},
+	{
+		href: "/cars/24-Porsche 992 GT3.html",
+		image: recent25,
+		title: "Porsche 992 GT3",
+	},
+	{
+		href: "/cars/25-fordgt40.html",
+		image: recent26,
+		title: "Ford GT40",
+	},
+	{
+		href: "/cars/26-Mercedes-Benz GTR AMG.html",
+		image: recent27,
+		title: "Mercedes-Benz AMG GTR",
+	},
+	{
+		href: "/cars/28-BMW i8 Spyder.html",
+		image: recent28,
+		title: "BMW i8 Spyder",
+	},
+	{
+		href: "/cars/29-Lotus Emira.html",
+		image: recent29,
+		title: "Lotus Emira",
+	},
+	{
+		href: "/cars/30-Ferrari GTC4Lusso.html",
+		image: recent30,
+		title: "Ferrari GTC4Lusso",
+	},
+	{
+		href: "/cars/31-BMW M3 CSL.html",
+		image: recent31,
+		title: "BMW M3 CSL",
+	},
+	{
+		href: "/cars/32-BMW M4 Competition.html",
+		image: recent32,
+		title: "BMW M4 Competition",
+	},
+	{
+		href: "/cars/33-Porsche Macan T.html",
+		image: recent33,
+		title: "Porsche Macan T",
+	},
+	{
+		href: "/cars/34-Ferrari 458 Italia.html",
+		image: recent34,
+		title: "Ferrari 458 Italia",
+	},
+	{
+		href: "/cars/35-Ford Mustang Mach 1.html",
+		image: recent35,
+		title: "Ford Mustang Mach 1",
+	},
+	{
+		href: "/cars/36 - Porsche 991.1 GT3RS.html",
+		image: recent36,
+		title: "Porsche 991.1 GT3RS",
+	},
+	{
+		href: "/cars/37-Ferrari 488 Pista.html",
+		image: recent37,
+		title: "Ferrari 488 Pista",
+	},
+	{
+		href: "/cars/39-Audi R8 V10+.html",
+		image: recent38,
+		title: "Audi R8 V10+",
+	},
+	{
+		href: "/cars/40-Audi R8 V10+ Spyder.html",
+		image: recent39,
+		title: "Audi R8 V10+ Spyder",
+	},
+	{
+		href: "/cars/41-Porsche 992 GT3.html",
+		image: recent40,
+		title: "Porsche 992 GT3",
+	},
+	{
+		href: "/cars/42-Audi RS6 Performance.html",
+		image: recent41,
+		title: "Audi RS6 Performance",
+	},
+	{
+		href: "/cars/43-Porsche 991.1 GT3RS.html",
+		image: recent42,
+		title: "Porsche 991.1 GT3RS",
+	},
+	{
+		href: "/cars/44-Mercedes-Benz SLS AMG.html",
+		image: recent43,
+		title: "Mercedes Benz SLS AMG",
+	},
+	{
+		href: "/cars/45-Ferrari 458 Speciale.html",
+		image: recent44,
+		title: "Ferrari 458 Speciale",
+	},
+	{
+		href: "/cars/46-Toyota Supra MkIV.html",
+		image: recent45,
+		title: "Toyota Supra MkIV",
+	},
+	{
+		href: "/cars/48 - Ferrari F12 TDF.html",
+		image: recent46,
+		title: "Ferrari F12 TDF",
+	},
+	{
+		href: "/cars/50-Ferrari F12.html",
+		image: recent47,
+		title: "Ferrari F12",
+	},
+	{
+		href: "/cars/51-BMW E90 M3.html",
+		image: recent48,
+		title: "BMW E90 M3",
+	},
+];
